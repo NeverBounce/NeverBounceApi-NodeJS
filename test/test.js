0b801c1cab4581a596c8c20659932e5b3d656f3d
Michael Mollick <mollick2@gmail.com>
2016-02-16 11:07:08 -0500

    Initial npm commit

---
+++ test/test.js
@@ -0,0 +1,10 @@
+// Just a dummy test for now...
+var assert = require('assert');
+describe('Array', function() {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal(-1, [1,2,3].indexOf(5));
+            assert.equal(-1, [1,2,3].indexOf(0));
+        });
+    });
+});
