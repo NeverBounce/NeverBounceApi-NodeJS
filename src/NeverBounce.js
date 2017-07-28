3385791bd90d25400ed903f11c1136100fdc352d
Mike Mollick <mollick2@gmail.com>
2017-07-28 16:39:38 -0400

    Migrating NeverBounce.js source to an ES6 class

--- src/NeverBounce.js
+++ src/NeverBounce.js
@@ -1,10 +1,73 @@
-// Load polyfill for older Node versions
-Object.assign = Object.assign || require('es6-object-assign').assign;
-var merge = require('lodash.merge');
+// Load lodash merge
+const merge = require('lodash.merge');
+
+// Load classes
+const resources = {
+    single: require('./Single'),
+};
+
+class NeverBounce {
+
+    /**
+     * Initializes NeverBounce object
+     * @param config
+     * @returns {NeverBounce}
+     * @constructor
+     */
+    constructor (config) {
+        this.config = merge({}, NeverBounce.defaultConfig, config);
+        this._prepResources();
+    }
+
+    /**
+     * Returns config
+     * @returns {*}
+     */
+    getConfig () {
+        return merge({}, this.config);
+    }
+
+    /**
+     * Make our https request options
+     * @param opts
+     * @returns {*}
+     */
+    getRequestOpts (opts) {
+        return merge({}, this.config.opts, opts);
+    }
+
+    /**
+     * Set api key
+     * @param key
+     */
+    setApiKey (key) {
+        this.config.apiKey = key;
+    }
+
+    /**
+     * Set api url
+     * @param host
+     */
+    setHost (host) {
+        this.config.opts.host = host;
+    }
+
+    /**
+     * Loads resources (endpoints) into object
+     * e.g. this.single && this.jobs
+     * @private
+     */
+    _prepResources() {
+        for (let name in resources) {
+            this[
+                name[0].toLowerCase() + name.substring(1)
+            ] = new resources[name](this);
+        }
+    }
+}
 
 /**
  * Default config values, these are overwritten when calling the constructor
- * @type {{apiKey: null, apiSecret: null, opts: {host: string, port: number, prefix: string, method: string, headers: {Content-Type: string, User-Agent: string}}}}
  */
 NeverBounce.defaultConfig = {
     apiKey: null,
@@ -19,80 +82,5 @@ NeverBounce.defaultConfig = {
         }
     }
 };
-
-var resources = {
-    single: require('./Single'),
-};
-
-/**
- * Initializes NeverBounce object
- * @param config
- * @returns {NeverBounce}
- * @constructor
- */
-function NeverBounce(config) {
-    if (!(this instanceof NeverBounce)) {
-        return new NeverBounce(config);
-    }
-
-    this.config = merge({}, NeverBounce.defaultConfig, config);
-    this._prepResources();
-}
-
-NeverBounce.prototype = {
-    /**
-     * Returns config
-     * @returns {*}
-     */
-    getConfig () {
-        return Object.assign({}, this.config);
-    },
-
-    /**
-     * Returns default config
-     * @returns {*}
-     */
-    getDefaultConfig () {
-        return Object.assign({}, NeverBounce.defaultConfig);
-    },
-
-    /**
-     * Make our https request options
-     * @param opts
-     * @returns {*}
-     */
-    getRequestOpts (opts) {
-        return Object.assign({}, this.config.opts, opts);
-    },
-
-    /**
-     * Set api key
-     * @param key
-     */
-    setApiKey (key) {
-        this.config.apiKey = key;
-    },
-
-    /**
-     * Set api url
-     * @param host
-     */
-    setHost (host) {
-        this.config.opts.host = host;
-    },
-
-    /**
-     * Loads resources (endpoints) into object
-     * e.g. this.Single && this.Jobs
-     * @private
-     */
-    _prepResources() {
-        for (var name in resources) {
-            this[
-                name[0].toLowerCase() + name.substring(1)
-            ] = new resources[name].client(this);
-        }
-    },
-}
 
 module.exports = NeverBounce;
