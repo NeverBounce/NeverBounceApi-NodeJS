509ce91548bb5ff344aa614c47b4adf099ae1adf
Mike Mollick <mollick2@gmail.com>
2017-08-01 11:25:07 -0400

    Adding VerificationObject & test

---
+++ src/VerificationObject.js
@@ -0,0 +1,75 @@
+class VerificationObject {
+
+    constructor (response) {
+        this.response = response;
+    }
+
+    /**
+     * Returns result
+     * @returns {Object}
+     */
+    getResult() {
+        return this.response.result;
+    }
+
+    /**
+     * Returns text code
+     * @returns {*}
+     */
+    getNumericResult() {
+        return VerificationObject.numericCodes[this.getResult()];
+    }
+
+    /**
+     * Returns true if result is of specified types
+     * @param codes
+     * @returns {boolean}
+     */
+    is(codes) {
+        if(Array.isArray(codes)) {
+            return (codes.indexOf(this.getResult()) > -1 || codes.indexOf(this.getNumericResult()) > -1);
+        } else {
+            return (codes === this.getResult() || codes === this.getNumericResult());
+        }
+    }
+
+    /**
+     * Returns true if result is NOT of specified types
+     * @param codes
+     * @returns {boolean}
+     */
+    not(codes) {
+        if(Array.isArray(codes)) {
+            return (codes.indexOf(this.getResult()) === -1 && codes.indexOf(this.getNumericResult()) === -1);
+        } else {
+            return (codes !== this.getResult() && codes !== this.getNumericResult());
+        }
+    }
+
+    /**
+     * Returns true if the flag was returned
+     * @param flag
+     * @returns {boolean}
+     */
+    hasFlag(flag) {
+        return this.response
+            && this.response.flags
+            && this.response.flags.indexOf(flag) > -1;
+    }
+}
+
+VerificationObject.valid = 0;
+VerificationObject.invalid = 1;
+VerificationObject.disposable = 2;
+VerificationObject.catchall = 3;
+VerificationObject.unknown = 4;
+
+VerificationObject.numericCodes = {
+    'valid': VerificationObject.valid,
+    'invalid': VerificationObject.invalid,
+    'disposable': VerificationObject.disposable,
+    'catchall': VerificationObject.catchall,
+    'unknown': VerificationObject.unknown,
+};
+
+module.exports = VerificationObject;
---
+++ test/VerificationObjectTest.js
@@ -0,0 +1,86 @@
+const assert = require('assert'),
+    VerificationObject = require('../src/VerificationObject');
+
+describe('VerificationObject', function () {
+    describe('Constructor', function () {
+        it('should set response object', function () {
+            const resp = {
+                result: 'valid',
+                flags: [],
+                suggested_correction: ''
+            };
+            const verification = new VerificationObject(resp);
+            assert.deepEqual(verification.response, resp);
+        });
+    });
+
+    describe('getResult/getNumericResult', function () {
+        it('return proper response', function () {
+            const resp = {
+                result: 'valid',
+                flags: [],
+                suggested_correction: ''
+            };
+            const verification = new VerificationObject(resp);
+
+            assert.equal(verification.getResult(), 'valid');
+            assert.equal(verification.getNumericResult(), 0);
+        });
+    });
+
+    describe('is', function () {
+        it('should return the true when in range, false when out', function () {
+            const resp = {
+                result: 'valid',
+                flags: [],
+                suggested_correction: ''
+            };
+            const verification = new VerificationObject(resp);
+
+            assert.equal(verification.is(0), true);
+            assert.equal(verification.is('valid'), true);
+            assert.equal(verification.is(VerificationObject.valid), true);
+            assert.equal(verification.is(1), false);
+            assert.equal(verification.is('invalid'), false);
+            assert.equal(verification.is([0, 3, 4]), true);
+            assert.equal(verification.is(['valid', 3, 4]), true);
+            assert.equal(verification.is([1, 2]), false);
+            assert.equal(verification.is(['invalid', 2]), false);
+        });
+    });
+
+    describe('not', function () {
+        it('should return the true when NOT in range, false when in', function () {
+            const resp = {
+                result: 'valid',
+                flags: [],
+                suggested_correction: ''
+            };
+            const verification = new VerificationObject(resp);
+
+            assert.equal(verification.not(0), false);
+            assert.equal(verification.not('valid'), false);
+            assert.equal(verification.not(VerificationObject.valid), false);
+            assert.equal(verification.not(1), true);
+            assert.equal(verification.not('invalid'), true);
+            assert.equal(verification.not([0, 3, 4]), false);
+            assert.equal(verification.not(['valid', 3, 4]), false);
+            assert.equal(verification.not([1, 2]), true);
+            assert.equal(verification.not(['invalid', 2]), true);
+        });
+    });
+
+    describe('hasFlag', function () {
+        it('should return the true when NOT in range, false when in', function () {
+            const resp = {
+                result: 'valid',
+                flags: ['bad_syntax'],
+                suggested_correction: ''
+            };
+            const verification = new VerificationObject(resp);
+
+            assert.equal(verification.hasFlag('bad_syntax'), true);
+            assert.equal(verification.hasFlag('has_dns'), false);
+        });
+    });
+});
