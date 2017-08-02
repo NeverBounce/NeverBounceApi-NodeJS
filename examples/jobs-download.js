5d147ae4cfa47c9bc06ada67cb8bb73d3a83b13a
Mike Mollick <mollick2@gmail.com>
2017-08-02 10:06:16 -0400

    Modifying examples to include query options

--- examples/jobs-download.js
+++ examples/jobs-download.js
@@ -3,8 +3,10 @@ const NeverBounce = require('../src/NeverBounce.js');
 // Initialize NeverBounce client
 const client = new NeverBounce(require('../.env'));
 
+const query = [];
+
 // Download a list
-client.jobs.download(285186).then(
+client.jobs.download(285186, query).then(
     resp => console.log(resp),
     err => console.log("ERROR: " + err.message)
 );
--- examples/jobs-results.js
+++ examples/jobs-results.js
@@ -4,7 +4,10 @@ const NeverBounce = require('../src/NeverBounce.js');
 const client = new NeverBounce(require('../.env'));
 
 // Get job results
-client.jobs.results(285186).then(
+client.jobs.results(285186,{
+//    'page': 1, // Page to start from
+//    'items_per_page': 10, // Number of items per page
+}).then(
     resp => {
         console.log("Results Count: " + resp.results.length);
         console.log("Results: " + resp.results.map(result => result.data.email + `(${result.verification.result})`));
