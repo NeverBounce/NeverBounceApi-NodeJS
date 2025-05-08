c3889ded1d5a080afe21e1e8bce6f692c839b216
Omri Katz <omri.katz@zoominfo.com>
2025-05-08 22:21:22 +0300

    chore(): fix jest config for github action

---
+++ jest.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/types.ts',
+    '!src/index.ts',
+  ],
+  setupFilesAfterEnv: ['./tests/setup.ts'],
+};
